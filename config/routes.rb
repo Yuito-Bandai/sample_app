4036d7b1d1f1d86b56074529b9500ac3ad533d85
Yuito-Bandai <yuiton1107@icloud.com>
2024-03-10 23:21:56 +0900

    よくわからないからいったんtop.html.erbに戻す

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "posts/index"=>"posts#index"
-  get "/"=>"home#index"
+  get "/"=>"home#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "profile"=>"home#profile"
   # Defines the root path route ("/")
