086e262846d24bf4a74fdd8f22b4c571650dee55
Yuito-Bandai <yuiton1107@icloud.com>
2024-03-10 22:24:55 +0900

    indexつけた

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "posts/index"=>"posts#index"
-  get "/"=>"home#top"
+  get "/"=>"home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "profile"=>"home#profile"
   # Defines the root path route ("/")
