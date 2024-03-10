b8df134620f4840383a1104991a157150c23192e
Yuito-Bandai <yuiton1107@icloud.com>
2024-03-10 21:36:37 +0900

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get "posts/index"=>"posts#index"
+  get "/"=>"home#top"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "profile"=>"home#profile"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
